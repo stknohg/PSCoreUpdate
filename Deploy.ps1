7250e9b00a3f51a0b00d1f68dcca1493610e1dc3
stknohg <stknohg@gmail.com>
2018-04-05 16:03:51 +0900

    add some messages at deploying.

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,6 +1,7 @@
 $RootPath = Join-Path $PSScriptRoot 'PSCoreUpdate'
 
 # Validate module manifest
+Write-Host 'Validate module manifest...' -ForegroundColor Green
 Test-ModuleManifest -Path (Join-Path $RootPath 'PSCoreUpdate.psd1') | Format-List
 if (-not $?) {
     Write-Error 'Module manifest validation was failed.'
@@ -8,6 +9,7 @@ if (-not $?) {
 }
 
 # Publish
+Write-Host 'Publish module...' -ForegroundColor Green
 if ($null -eq $env:NUGET_API_KEY) {
     Write-Error 'NUGET_API_KEY environment variable not found.'
     exit 
