af6528c2fde09ff664a974bb5a87037225e2f521
stknohg <stknohg@gmail.com>
2018-04-26 12:16:13 +0900

    Add tests.

--- PSCoreUpdate.Tests/Test-LatestVersion.Tests.ps1
+++ PSCoreUpdate.Tests/Test-LatestVersion.Tests.ps1
@@ -14,8 +14,23 @@ Describe "Test-LatestVersion unit tests" {
         { Test-LatestVersion -Token $Token } | Should -Not -Throw
     }
 
+    It "Test-LatestVersion -ExcludePreRelease exit normaly" {
+        { Test-LatestVersion -ExcludePreRelease -Token $Token } | Should -Not -Throw
+    }
+
     It "Test-LatestVersion -PassThru returns a result object" {
         $result = Test-LatestVersion -Token $Token -PassThru
+        $result | Should -Not -BeNullOrEmpty
+        $result.Result | Should -BeOfType 'bool'
+        if ($result.Result) {
+            $result.Release | Should -BeNullOrEmpty
+        } else {
+            $result.Release | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    It "Test-LatestVersion -ExcludePreRelease -PassThru returns a result object" {
+        $result = Test-LatestVersion -ExcludePreRelease -Token $Token -PassThru
         $result | Should -Not -BeNullOrEmpty
         $result.Result | Should -BeOfType 'bool'
         if ($result.Result) {
