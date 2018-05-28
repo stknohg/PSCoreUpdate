32c108b19fd77485680fe767490c5a08f2b5a0ca
stknohg <stknohg@gmail.com>
2018-05-28 17:03:10 +0900

    dded feature that notifies the new version releasing is coming.

--- PSCoreUpdate/Test-LatestVersion.ps1
+++ PSCoreUpdate/Test-LatestVersion.ps1
@@ -23,6 +23,7 @@ function Test-LatestVersion {
     if ($PSVersionTable.PSVersion -gt $release.Version) {
         # Note : This pattern occurs when using -ExcludePreRelease parameter.
         WriteInfo ('PowerShell Core {0} is newer than the latest version {1}.' -f $PSVersionTable.PSVersion, $release.Version)
+        NotifyNewVersion -Published $release.Published -ExcludePreRelease $ExcludePreRelease
         if ($PassThru) {
             return [PSCustomObject]@{ Result = $true; Release = $release }
         }
@@ -30,6 +31,7 @@ function Test-LatestVersion {
     }
     if ($PSVersionTable.PSVersion -eq $release.Version) {
         WriteInfo ('No updates. PowerShell Core {0} is the latest version.' -f $PSVersionTable.PSVersion)
+        NotifyNewVersion -Published $release.Published -ExcludePreRelease $ExcludePreRelease
         if ($PassThru) {
             return [PSCustomObject]@{ Result = $true; Release = $release }
         }
@@ -38,5 +40,18 @@ function Test-LatestVersion {
     Write-Warning ('Newer version PowerShell Core {0} is available.' -f $release.Version)
     if ($PassThru) {
         return [PSCustomObject]@{ Result = $false; Release = $release }
+    }
+}
+
+function NotifyNewVersion ([datetime]$Published, [boolean]$ExcludePreRelease) {
+    # Note : Notify the new version releasing is coming.
+    #  * Stable Release : after a half year (180 days)
+    #  * PreRelease : after 3 weeks (21 days)
+    $span = if ($ExcludePreRelease) {[timespan]::new(180, 0, 0, 0)} else {[timespan]::new(21, 0, 0, 0)}
+    $elapsed = (Get-Date).Subtract($Published)
+    if ($elapsed -ge $span) {
+        WriteInfo ""
+        WriteInfo ("{0} days have elapsed since the latest PowerShell Core was released." -f $elapsed.Days)
+        WriteInfo "A new version may be released in the near future."
     }
 }
